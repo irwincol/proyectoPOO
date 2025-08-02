d2aac4d5e3257b8a6283fc0b177fe6092a613fc4
irwincol <irwincolmenarez10@gmail.com>
2025-08-02 12:54:23 -0500

    usecase

--- src/tasks/models/useCases/TaskUseCase.java
+++ src/tasks/models/useCases/TaskUseCase.java
@@ -4,10 +4,44 @@ import tasks.models.Task;
 
 public class TaskUseCase {
 
-    public void create (String name, String description) {
-        Task task = new Task(name, description, status: false);
+    // Propiedad
+    Task task;
+
+    //Constructor
+
+    public TaskUseCase(){
+        this.task = new Task();  //Forma 
+    
+
+    public String create (String name, String description) {
+        Task task = new Task(name, description,  false);
         return "Tarea Creada:" + task.getName() + "Descripcion" + task.getDescription();
 
         
     }
+
+    //Update
+
+    public task.set 
+
+
+
+    //Delete
+
+
+
+
+    //  Get
+
+    public String get(){
+         return "Tarea Creada:" + task.getName() + "Descripcion" + task.getDescription();
+    
+    }
+
+
+
+
+
+
+
 }
